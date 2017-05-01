e566dd7949751a69ae4b71bd22fc0145c89ee87f
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2017-05-01 17:32:16 +0200

    did things

--- 15_CDIO3/src/presentationLayer/UserService.java
+++ 15_CDIO3/src/presentationLayer/UserService.java
@@ -1,18 +1,23 @@
 package presentationLayer;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import DTO.UserDTO;
 
 @Path("json")
 public class UserService {
 	ObjectMapper mapper = new ObjectMapper();
+	private static ArrayList<UserDTO> allUsers = new ArrayList<UserDTO>() {
+		private static final long serialVersionUID = -6607099362887584352L;
+	};
 	 
 	@Path("createUser")
 	@POST
@@ -22,7 +27,7 @@ public class UserService {
 		try {
 			UserDTO res = mapper.readValue(json, UserDTO.class);
 			System.out.println(res.toString());
-			
+			allUsers.add(res);
 			return "User received.";
 //			return null;
 		} catch (IOException e) {
@@ -30,5 +35,24 @@ public class UserService {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	@Path("loadUsers")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<UserDTO> getUserList(){
+		return allUsers;
+	}
+	
+	@Path("deleteUser")
+	@DELETE
+	public boolean delete(UserDTO request) {
+		for (int i = 0; i<allUsers.size();i++) {
+			if (allUsers.get(i).getUserId() == request.getUserId()) {
+				allUsers.remove(i);
+				return true;
+			}
+		}
+		return false;
 	}
 }
